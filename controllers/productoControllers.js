b53d353a9dde709ac332d30a92d406f4a04bb3c8
Zamir31 <noriegad258@gmail.com>
2023-04-03 12:00:37 -0600

    CRUD backend terminado

--- controllers/productoControllers.js
+++ controllers/productoControllers.js
@@ -14,3 +14,74 @@ exports.crearProducto = async (req, res) => {
         res.status(500).send(error.message);
     }
 };
+
+
+exports.obtenerProductos = async (req, res) => {
+
+    try {
+        
+        const productos = await Producto.find();
+        res.json(productos);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+}
+
+exports.actualizarProducto = async (req, res) => {
+    try {
+
+        const { nombreProducto, categoria, ubicacion, precio } = req.body;
+        let producto = await Producto.findById(req.params.id);
+
+        if (!producto) {
+            res.status(404).json({ message: "Producto no existe" });
+        }
+
+        producto.nombreProducto = nombreProducto;
+        producto.categoria = categoria;
+        producto.ubicacion = ubicacion;
+        producto.precio = precio;
+
+        producto = await Producto.findOneAndUpdate({ _id: req.params.id }, producto, { new: true});
+        res.json(producto);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+};
+
+exports.obtenerProducto = async (req, res) => {
+
+    try {
+
+        let producto = await Producto.findById(req.params.id);
+        if (!producto) {
+            res.status(404).json({ message: "Producto no existe" });
+        }
+        res.json(producto);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+};
+
+exports.deletearProducto = async (req, res) => {
+    try {
+
+        let producto = await Producto.findById(req.params.id);
+        if (!producto) {
+            res.status(404).json({ message: "Producto no existe" });
+        }
+
+        await Producto.findOneAndRemove({ _id: req.params.id });
+        res.json({ message: "Producto eliminado"});
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+};
--- routes/producto.js
+++ routes/producto.js
@@ -5,5 +5,9 @@ const router = express.Router();
 const productoController = require('../controllers/productoControllers')
 
 router.post('/', productoController.crearProducto);
+router.get('/', productoController.obtenerProductos);
+router.get('/:id', productoController.obtenerProducto);
+router.put('/:id', productoController.actualizarProducto);
+router.delete('/:id', productoController.deletearProducto);
 
 module.exports = router;
