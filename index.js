c14fff29ef89e5d5453f66ccd849fd5b9086dfa4
Zamir31 <noriegad258@gmail.com>
2023-04-12 12:19:39 -0600

    terminado

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const conectarDB = require('./config/db');
+const cors = require("cors");
 
 //creamos el servidor
 const app = express();
 
 //conectamos a la base de datos
 conectarDB();
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/producto', require('./routes/producto'));
